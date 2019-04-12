601dfe4ff9f5d0a60a73742c68443d77f364a1ff
EmaSuriano <emanuel.suriano@gmail.com>
2019-04-12 12:16:45 +0200

    Updates

---
+++ precache-manifest.5cbaeae7567d808c38d23e737b7d57e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "614f22ee769db1c15de0",
+    "url": "/react-amsterdam-challenge-2019/static/js/runtime~main.35aaa403.js"
+  },
+  {
+    "revision": "2a2948d147c2f4298ebf",
+    "url": "/react-amsterdam-challenge-2019/static/js/main.5ed39a9d.chunk.js"
+  },
+  {
+    "revision": "9c23ae6150a9376d6d50",
+    "url": "/react-amsterdam-challenge-2019/static/js/2.4f26d29b.chunk.js"
+  },
+  {
+    "revision": "2a2948d147c2f4298ebf",
+    "url": "/react-amsterdam-challenge-2019/static/css/main.88f2c982.chunk.css"
+  },
+  {
+    "revision": "fa0a9645a2445d5d89f54bcb9e6d7a77",
+    "url": "/react-amsterdam-challenge-2019/index.html"
+  }
+];
